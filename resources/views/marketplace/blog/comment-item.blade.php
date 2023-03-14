d05b1c331d3c362ada214fcedb7533af0b9f3765
rosecontee <rosecontee@outlook.com>
2023-03-14 15:20:07 +0100

    2023-03-14

--- resources/views/marketplace/blog/comment-item.blade.php
+++ resources/views/marketplace/blog/comment-item.blade.php
@@ -1,7 +1,6 @@
 <div class="blog-comments-item">
     <hr>
     <p class="m-0">Name: <b>{{ $comment['name'] }}</b></p>
-    <p class="m-0">Email: <b>{{ $comment['email'] }}</b></p>
-    <p>This is my comments.</p>
+    <p>{{ $comment['comment'] }}</p>
     <p class="small m-0">{{ date('F j, Y g:i A', strtotime($comment['created_at'])) }}</p>
 </div>
